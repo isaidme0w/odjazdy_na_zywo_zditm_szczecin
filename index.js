f07e4e7504bfbdc4412130295e83a86391ff7dd8
isaidme0w <piotr.garda11@gmail.com>
2025-03-23 21:39:38 +0100

    Dodane wsparcie bootstrap 5 (modyfikacja interfejsu), lekko zmodyfikowany backend, Dodano informację o przystankach 'na żądanie'

--- index.js
+++ index.js
@@ -9,14 +9,20 @@ app.use(express.json());
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+let stopsData = {};
+
 app.get('/', async (req, res) => {
     try {
         const response = await axios.get(`${BASE_URL}/stops`);
-        const stops = response.data.data.map(stop => ({
-            id: stop.id,
-            number: stop.number,
-            name: stop.name
-        }));
+        const stops = response.data.data.map(stop => {
+            stopsData[stop.number] = { name: stop.name, request_stop: stop.request_stop };
+            return {
+                id: stop.id,
+                number: stop.number,
+                name: stop.name,
+                request_stop: stop.request_stop
+            }
+        });
         res.render('stops', { stops });
     } catch (error) {
         res.status(500).send('Błąd podczas pobierania listy przystanków');
@@ -27,7 +33,13 @@ app.get('/departures/:stopNumber', async (req, res) => {
     const { stopNumber } = req.params;
     try {
         const response = await axios.get(`${BASE_URL}/displays/${stopNumber}`);
-        res.render('departures', { stop: response.data });
+        const stopInfo = response.data;
+        if (stopsData[stopNumber]) {
+            stopInfo.request_stop = stopsData[stopNumber].request_stop;
+        } else {
+            stopInfo.request_stop = false;
+        }
+        res.render('departures', { stop: stopInfo });
     } catch (error) {
         res.status(500).send('Błąd podczas pobierania odjazdów dla przystanku');
     }
